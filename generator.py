95947db5b906f54510a7aec88562968076b46c92
Raad Altaie <raad.altaie@icloud.com>
2018-05-22 18:51:22 -0700

    first commit

---
+++ generator.py
@@ -0,0 +1,20 @@
+import random
+
+#Generate Random DNA Sequence
+
+def random_dna_sequence(length):
+    return ''.join(random.choice('ACTG') for _ in range(length))
+
+
+
+#DNA sequences with equal base probability
+
+def base_frequency(dna):
+    D = {}
+    for base in 'ATCG':
+        D[base] = dna.count(base)/float(len(dna))
+    return D
+
+for _ in range(20):
+    dna = random_dna_sequence(50)
+    print (dna, base_frequency(dna))
